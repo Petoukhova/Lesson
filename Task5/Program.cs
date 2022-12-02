f7743942c3e0579c07b91739de45338cc71dc460
Helene Petoukhova <helene-7@mail.ru>
2022-12-02 14:05:22 +0500

    Output of array

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// This script will work only for positive number
+// For negative number need to turn over condition
+// The best way is to use the number module, but I don't know this operator for this language
+
+
+Console.Clear();
+Console.WriteLine("Please, enter the number");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = -n;
+
+while (i <= n)
+{
+   Console.Write($"{i} "); 
+   i++;
+}
