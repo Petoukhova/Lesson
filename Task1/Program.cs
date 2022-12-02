0ec7b582694fb49c31705f6ace8de3441d6e36cb
Helene Petoukhova <helene-7@mail.ru>
2022-12-02 13:21:25 +0500

    Checking the box

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Please, enter the square of the number");
+
+int q = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please, enter the number to be checked");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (q == n * n)
+    Console.WriteLine("Profit!");
+else
+    Console.WriteLine("Mistake...");
