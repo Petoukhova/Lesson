05d0ac2909781c2bf2cb1ed0ba7fed25f5c4aa69
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 17:00:09 +0500

    Checking multiplicity

---
+++ Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter the number");
+
+int n = Convert.ToInt32 (Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+
+Console.WriteLine("Profit!");
+
+else 
+Console.WriteLine("Mistake... ");
