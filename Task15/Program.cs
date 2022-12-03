c3aa88586e4e8e121492c52e329c7b50fdfdd58b
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 17:34:36 +0500

    Holiday

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Please enter the day of the week (as a number)");
+
+int d = Convert.ToInt32(Console.ReadLine());
+
+
+while (d < 1 || d > 7) 
+{
+    Console.Write("Mistake... Try again!");
+    d = Convert.ToInt32(Console.ReadLine());
+}
+
+if (d == 6 || d ==7)
+    Console.WriteLine("Holiday!!!");
+else 
+    Console.WriteLine("Work day...");
