8172348f0cd58d19aa1fb0cc830f7c1ccbc9993a
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 16:56:33 +0500

    Div two numbers

---
+++ Task12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Enter two numbers");
+
+int n1 = Convert.ToInt32 (Console.ReadLine());
+
+int n2 = Convert.ToInt32 (Console.ReadLine());
+
+if (n1 % n2 == 0)
+
+Console.WriteLine("Profit!");
+
+else 
+Console.WriteLine($"Mistake... {n1 % n2}");
