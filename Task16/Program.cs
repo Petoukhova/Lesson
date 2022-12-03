2158cfa618afbd1f3fa7218987b69d55dc018d37
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 17:18:17 +0500

    Two numbers are square

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Enter two numbers");
+
+int n1 = Convert.ToInt32 (Console.ReadLine());
+
+int n2 = Convert.ToInt32 (Console.ReadLine());
+
+if (n1 * n1 == n2 || n2*n2 == n1)
+
+Console.WriteLine("Profit!");
+
+else 
+Console.WriteLine($"Mistake... ");
