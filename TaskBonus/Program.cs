c773f20935431f2d11c0ef0a11c9eec588608906
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 19:57:17 +0500

    The second maximum

---
+++ TaskBonus/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("You need to enter minimum two positive numbers (except 0) otherwise the task does not make sense");
+
+int max1 = Convert.ToInt32(Console.ReadLine());
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int max2 = 0;
+
+if (n < max1)
+    max2 = n;
+else {
+    max2 = max1;
+    max1 = n;
+}
+
+while (n !=0)
+{ n = Convert.ToInt32(Console.ReadLine());
+    if (n > max2 && n < max1)
+    max2 = n;
+    else if (n > max1)
+    max2 = max1;
+    max1 = n;
+}
+
+Console.WriteLine($"The second maximum is {max2}");
