994af24e7bcf48c0f872ad62d1f220cd47931d24
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 12:57:44 +0500

    Even parity

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Please, enter the checking number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n%2 == 0)
+    Console.WriteLine("Profit! This number is even!");
+else 
+    Console.WriteLine("I'm sorry, but your number isn't even...");
