a10d72585eec6ed0f389aa664bc75a7e4cfa8c1d
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 12:41:11 +0500

    Compare two numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Please, enter the first number: ");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please, enter the second number: ");
+
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 == n2) {
+
+Console.WriteLine("The both number are equal");
+
+}
+
+else if (n1 > n2) {
+
+Console.WriteLine($"The biggest number is {n1}");
+Console.WriteLine($"The smallest number is {n2}");
+}
+else {
+Console.WriteLine($"The biggest number is {n2}");
+Console.WriteLine($"The smallest number is {n1}");
+}
