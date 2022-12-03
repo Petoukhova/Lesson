f2fcb014a11e561f2fa5d4dce73462c921f19fe4
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 18:50:04 +0500

    The third digit

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Enter the positive number");
+
+int n = Convert.ToInt32 (Console.ReadLine());
+
+if (n < 100)
+
+Console.WriteLine("This number doesn't have third digit");
+
+else {
+    while (n > 999)
+    {n = n / 10;
+ //   Console.WriteLine(n);
+ }
+    
+    {n = n % 10;
+    Console.WriteLine($"The third digit is {n}");}
+    
+}
+
+
