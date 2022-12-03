c63849a4d5a3988bb98ed85c867e237f9b2a259f
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 13:13:31 +0500

    Output even numbers

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Please, enter the number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n <2) {
+
+    Console.Write("You entered too little number... Try again! ");
+
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 2; i <=n; i = i + 2)
+{
+    Console.Write($"{i} ");
+}
