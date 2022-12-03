e434be13ab129292a038bd545b73800db51af4db
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 12:50:38 +0500

    Compare three numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Please, enter the first number: ");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please, enter the second number: ");
+
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please, enter the third number: ");
+
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n2 > max) 
+    max = n2;
+
+if (n3 > max)
+    max = n3;
+
+Console.WriteLine($"The biggest number is {max}");
