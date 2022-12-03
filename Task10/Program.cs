672d5454cce15d5a227dc17ebf3be74512eedaaf
Helene Petoukhova <helene-7@mail.ru>
2022-12-03 17:29:35 +0500

    The second digit

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a three-digit number");
+
+int n = Convert.ToInt32 (Console.ReadLine());
+
+int n1 = n / 10;
+
+int total = n1 % 10;
+
+Console.WriteLine($"The second digit is - {total}");
