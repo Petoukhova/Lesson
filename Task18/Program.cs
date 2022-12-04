89b90daf4594f45e89d3698c195c31209b8d6d5f
Helene Petoukhova <helene-7@mail.ru>
2022-12-04 16:20:02 +0500

    Quaters

---
+++ Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Enter two quater (in arabic style)");
+
+int n = Convert.ToInt32 (Console.ReadLine());
+
+while (n <1 || n >4)
+{
+Console.WriteLine("Mistake... Try again!");
+n = Convert.ToInt32 (Console.ReadLine());
+}
+
+if (n == 1)
+
+Console.WriteLine("X > 0; Y > 0");
+
+else if (n == 2)
+Console.WriteLine("X < 0; Y > 0");
+
+else if (n == 3)
+Console.WriteLine("X < 0; Y < 0");
+
+else if (n == 4)
+Console.WriteLine("X > 0; Y < 0");
