d65cf46dc6f50bfe246772e57e5219945a2d5863
Helene Petoukhova <helene-7@mail.ru>
2022-12-07 20:51:50 +0500

    3D distance

---
+++ Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Enter coordinetas of two points:");
+
+Console.Write("A - X: ");
+
+int aX = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("A - Y: ");
+
+int aY = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("A - Z: ");
+
+int aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("B - X: ");
+
+int bX = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("B - Y: ");
+
+int bY = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("B - Z: ");
+
+int bZ = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(aX - bX, 2) + Math.Pow(aY - bY, 2) + Math.Pow(aZ - bZ, 2)), 2));
