8c2544e14d8339231664e2d40686307e3e0272d5
Helene Petoukhova <helene-7@mail.ru>
2022-12-07 20:55:24 +0500

    The third degree

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+for (i=1; i<=n; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
