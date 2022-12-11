a1089527edebf94e7e4308b55133ff7e4551e08d
Helene Petoukhova <helene-7@mail.ru>
2022-12-11 23:28:53 +0500

    Array output

---
+++ Task30/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(0, 2); // [1, 10]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
