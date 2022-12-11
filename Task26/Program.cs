7e73a0b8b355da53556383813f72b61796c96192
Helene Petoukhova <helene-7@mail.ru>
2022-12-11 23:18:16 +0500

    Coutn of digits

---
+++ Task26/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+
+while (n > 0)
+{
+    n /= 10;
+    count++;
+}
+Console.WriteLine(count);
