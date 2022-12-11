ec06a77edec46e35aef30430a18879eaf2e465ec
Helene Petoukhova <helene-7@mail.ru>
2022-12-11 23:25:00 +0500

    Factorial

---
+++ Task28/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), res = 1;
+
+for (int i = 2; i <= n; i++)
+    res *=  i;
+
+Console.WriteLine(res);
+
