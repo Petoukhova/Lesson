a5ec51488ab2ef899f7cd6521e83d172cab09883
Helene Petoukhova <helene-7@mail.ru>
2022-12-11 23:37:27 +0500

    Logarithm

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите основание: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int s = Convert.ToInt32(Console.ReadLine()), res = n;
+
+for (int i = 2; i <= s; i++)
+    res = res * n;
+
+Console.WriteLine(res);
+
+
