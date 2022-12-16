2fc0c76b8c3ee838e369dd180871456e603565ec
Helene Petoukhova <helene-7@mail.ru>
2022-12-16 21:13:27 +0500

    Massive

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// код работает, но при попытке создать числовой массив путем конвертирования имеющегося выдало ошибку
+// Unhandled exception. System.IndexOutOfRangeException: Index was outside the bounds of the array.
+//   at Program.<Main>$(String[] args) in D:\CSharp\Lesson1\Task29\Program.cs:line 14//
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элементы массива: ");
+string [] array = Console.ReadLine().Split(", ");
+/*
+int [] total = new int [array.Length-1];
+
+for (int i = 0; i < array.Length; i++)
+    total[i] = int.Parse(array[i]);
+*/
+Console.WriteLine($"[{string.Join(", ", array)}]");
