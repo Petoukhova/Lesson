de899237b3cc8467503cb45d537220a16f512eee
Helene Petoukhova <helene-7@mail.ru>
2022-12-17 20:20:41 +0500

    Triangle

---
+++ Task40/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите стороны треугольника: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+if (array[0] < array[1]+array[2] && array[1]< array[0]+array[2] && array[2]<array[0]+array[1])
+Console.Write("Да");
+else Console.Write("Нет");
