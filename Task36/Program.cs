d329b3ffd58df418baf3ed2f88f9b73b6c395285
Helene Petoukhova <helene-7@mail.ru>
2022-12-17 15:45:24 +0500

    Sum of not even elements

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1,11);
+}
+
+void ReleaseArray (int [] array)
+{
+    int count = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        count = count + array[i];
+    }
+    Console.WriteLine($"Сумма элементов на нечетных позициях: {count}");
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+InputArray (array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray (array);
