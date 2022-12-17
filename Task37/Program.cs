76db0d311a5321406a614ba0ab3c74facc9578cd
Helene Petoukhova <helene-7@mail.ru>
2022-12-17 15:28:21 +0500

    multiplication

---
+++ Task37/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1,20);
+}
+
+void ReleaseArray (int [] array)
+{
+    for (int i = 0; i < array.Length /2 + array.Length%2; i++)
+    Console.Write($"{array [i] * array [array.Length-1-i]} ");
+    
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+InputArray (array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray (array);
