c0e7030e13de691047c2ee63a9bb9c5ae7932694
Helene Petoukhova <helene-7@mail.ru>
2022-12-20 17:20:23 +0500

    Intersection point

---
+++ Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите К1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите К2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите B1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите B2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = ((b2-b1)/(k1-k2));
+double y = k1 * ((b2-b1)/(k1-k2)) + b1;
+
+Console.Write($"Точка пересечения: {x}, {y}");
