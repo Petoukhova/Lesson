f1bea8fb57359430a0990d62424cc32026bef15c
Helene Petoukhova <helene-7@mail.ru>
2022-12-23 22:15:33 +0500

    Matrix of double

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -16,12 +16,3 @@ Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 double[,] matrix = new double[size[0], size[1]];
 InputMatrix(matrix);
-Console.Write("Введите позицию элемента: ");
-int[] point = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-
-if (point[0]>=0 && point[0]<matrix.GetLength(0))
-{
-    if (point[1]>=0 && point[1]<matrix.GetLength(1))
-        Console.WriteLine (matrix [point[0], point[1]]);
-    else Console.WriteLine ("Данная позиция в матрице не существует");  
-}
