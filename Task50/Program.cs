a44539c6be7b608351c17c6a44ac7a9a394de9a1
Helene Petoukhova <helene-7@mail.ru>
2022-12-23 22:14:20 +0500

    Position of point

---
+++ Task50/Program.cs
@@ -0,0 +1,27 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.Write("Введите позицию элемента: ");
+int[] point = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+if (point[0]>=0 && point[0]<matrix.GetLength(0))
+{
+    if (point[1]>=0 && point[1]<matrix.GetLength(1))
+        Console.WriteLine (matrix [point[0], point[1]]);
+    else Console.WriteLine ("Данная позиция в матрице не существует");  
+}
